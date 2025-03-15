550449191ed8ed1eb404ad5c65e3124725fa60cb
Jun127 <convertking12345@gmail.com>
2025-03-15 16:09:56 +0800

    修正 Flask 綁定 PORT

--- app.py
+++ app.py
@@ -40,4 +40,8 @@ def order():
     return jsonify({"success": False, "message": "庫存不足"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+   import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=True)
+
